afec38e3a2517bcbd2e38c9ee770e8842aeb7fc1
alfitodangkuhono <fitodangkuhono@gmail.com>
2025-10-13 14:50:21 +0700

    feat: LengthOfLastWord

---
+++ LengthOfLastWord.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func LengthOfLastWord(s string) int {
+
+	var char_flag bool // digunakan untuk trigger jika sudah menemukan karakter bukan spasi
+	var length int = 0 // panjang karakter
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] != ' ' { // validasi per karakter tidak boleh spasi
+			char_flag = true //  deklarasi sudah menemukan karakter
+			length++         //menambhkan value dari variable length satu per satu
+		} else {
+			if char_flag { // ketika triggernya benar maka langsung return length
+				return length
+			}
+		}
+	}
+	return length
+}
+
+func main() {
+	fmt.Println(LengthOfLastWord("Hello Worl d "))
+}
